b83aefc7b3fe48c79c4dfa9f67162238c4bd44da
Martin Cravero <cravero.martin@gmail.com>
2020-10-09 14:20:41 -0300

    update site

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,21 +43,21 @@ const IndexPage = () => (
         <img src={Causas} alt="Causas"/>
         <h2>NUESTRAS CAUSAS</h2>
         <p>Nuestro compromiso es propiciar el impulso de los Objetivos de Desarrollo Sostenible de Naciones Unidas, con la misión de contribuir a salvar vidas y aliviar sufrimiento de poblaciones vulnerables. Somos una fuerza conjunta equipada para lidiar con Emergencias Sociales de todo tipo. </p>
-      <Link to='/' className={styles.link}>DESCUBRA MÁS</Link>
+      <Link to='/causas' className={styles.link}>DESCUBRA MÁS</Link>
       </div>
       <div className={styles.sectionFive}>
         <img src={NuestrosServicios} alt="Nuestros servicios"/>
         <h2>NUESTROS SERVICIOS</h2>
         <p>GRAES se centra en la consultoría integral de desarrollo sostenible en Emergencias Sociales. Nos especializamos en la investigación, fabricación de tecnologías, ejecución de proyectos y formulación de propuestas relacionadas con afectaciones y riesgos en comunidades vulnerables. </p>
         <p>El abordaje de proyectos abarca desde las fases tempranas de diagnóstico y prevención en contextos de Emergencias, a las etapas de contención temprana y respuesta para atender Crisis en desarrollo. Proporcionamos a nuestros clientes, los modelos y herramientas necesarias para la gestión de este tipo de proyectos en todas sus etapas.</p>
-        <Link to='/' className={styles.link}>VER TODOS</Link>
+        <Link to='/servicios' className={styles.link}>VER TODOS</Link>
       </div>
       <img src={RespuestaCovidFondo} alt="Respuesta COVID" className={styles.respuestaCovidFull}/>
       <div className={styles.sectionSix}>
         <img src={RespuestaCovid} alt="Respuesta COVID"/>
         <h2>RESPUESTA ANTE COVID 19</h2>
         <p>La crisis global generada por la Pandemia del COVID-19 amenaza a comunidades vulnerables y revierte los logros de desarrollo sostenible. Conozca nuestra respuesta activa y directa en la lucha contra la Pandemia.</p>
-        <Link to='/' className={styles.link}>VER IMPACTO</Link>
+        <Link to='/covid19' className={styles.link}>VER IMPACTO</Link>
       </div>
       <Aliados/>
     </div>
